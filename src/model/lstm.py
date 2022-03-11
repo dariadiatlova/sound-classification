a8a8d0afefcbd90047b1275b8c40915e839ce6b7
diatlova <dari.diatlova@gmail.com>
2022-03-11 14:11:59 +0300

    updated training config updated trainign config

--- src/model/lstm.py
+++ src/model/lstm.py
@@ -8,13 +8,13 @@ class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout):
         """
             Abstract class for LSTM cells.
-            :param input_dim: expected number of features, mel freq_bands, 64.
+            :param input_dim: expected number of features, mel freq_bands, 128.
             :param hidden_dim: int dimensionality for hidden layers, 256.
             :param output_dim: int number of classes, 10.
             :param num_layers: int number of lstm layers.
         """
         super(LSTM, self).__init__()
-        # input: [batch_size, mel_freq_bands, time_steps], [16, 64, 344]
+        # input: [batch_size, mel_freq_bands, time_steps], [16, 128, 344]
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout)
         self.linear = nn.Linear(hidden_dim, output_dim)
 
