3fff7b6b9aa5518a1acaacfe1698bb0edad5a90d
diatlova <dari.diatlova@gmail.com>
2022-03-13 17:38:54 +0300

    configured lstm params

--- src/model/lstm.py
+++ src/model/lstm.py
@@ -8,7 +8,7 @@ class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout, inference: bool = False):
         """
             Abstract class for LSTM cells.
-            :param input_dim: expected number of features, mel freq_bands, 128.
+            :param input_dim: expected number of features, mel freq_bands, 64.
             :param hidden_dim: int dimensionality for hidden layers, 256.
             :param output_dim: int number of classes, 10.
             :param num_layers: int number of lstm layers.
@@ -17,14 +17,13 @@ class LSTM(nn.Module):
         """
         super(LSTM, self).__init__()
         self.inference = inference
-        # input: [batch_size, mel_freq_bands, time_steps], [16, 128, 344]
+        # input: [batch_size, mel_freq_bands, time_steps], [512, 64, 344]
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout)
         self.linear = nn.Linear(hidden_dim, output_dim)
         self.softmax = nn.Softmax(-1)
 
-    def forward(self, x: torch.Tensor, hidden: Optional[torch.Tensor] = None) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x.permute(0, 2, 1)
-        # non-stateful hidden as we do not implement any inference here
         if not self.inference:
             lstm_out, _ = self.lstm(x)
             logits = self.linear(lstm_out[:, -1, :])
